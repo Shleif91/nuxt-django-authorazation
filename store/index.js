1e1156dfb058e64e88ee89ed7d52fdab387cd89b
Shleif91 <shleikooleg@gmail.com>
2018-03-21 14:00:48 +0300

    work with errors

--- store/index.js
+++ store/index.js
@@ -1,7 +1,17 @@
 import * as firebase from 'firebase'
 
 export const state = () => ({
-  user: null
+  user: null,
+  loading: false,
+  error: null,
+  totalPages: 1,
+  orders: [
+    // {id: 1, title: 'Яблоки срочно', cityFrom: 'Минск', cityWhere: 'Москва', cargo: 'Яблоки'},
+    // {id: 2, title: 'Окна срочно', cityFrom: 'Москва', cityWhere: 'Минск', cargo: 'Окна'},
+    // {id: 3, title: 'Окна срочно', cityFrom: 'Москва', cityWhere: 'Минск', cargo: 'Окна'},
+    // {id: 4, title: 'Окна срочно', cityFrom: 'Москва', cityWhere: 'Минск', cargo: 'Окна'},
+    // {id: 5, title: 'Окна срочно', cityFrom: 'Москва', cityWhere: 'Минск', cargo: 'Окна'}
+  ]
 })
 
 export const getters = {
@@ -9,47 +19,60 @@ export const getters = {
     return state.user
   },
   orders (state) {
-    return [
-      // {id: 1, title: 'Яблоки срочно', cityFrom: 'Минск', cityWhere: 'Москва', cargo: 'Яблоки'},
-      // {id: 2, title: 'Окна срочно', cityFrom: 'Москва', cityWhere: 'Минск', cargo: 'Окна'},
-      // {id: 3, title: 'Окна срочно', cityFrom: 'Москва', cityWhere: 'Минск', cargo: 'Окна'},
-      // {id: 4, title: 'Окна срочно', cityFrom: 'Москва', cityWhere: 'Минск', cargo: 'Окна'},
-      // {id: 5, title: 'Окна срочно', cityFrom: 'Москва', cityWhere: 'Минск', cargo: 'Окна'}
-    ]
+    return state.orders
   },
-  totalPages () {
-    return 5
+  totalPages (state) {
+    return state.totalPages
   }
 }
 
 export const mutations = {
   setUser (state, payload) {
     state.user = payload
+  },
+  setLoading (state, payload) {
+    state.loading = payload
+  },
+  setError (state, payload) {
+    state.error = payload
+  },
+  clearError (state) {
+    state.error = null
   }
 }
 
 export const actions = {
   signUserUp ({commit}, payload) {
+    commit('setLoading', true)
+    commit('clearError')
     firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
       .then(user => {
+        commit('setLoading', false)
         const newUser = {
           id: user.uid
         }
         commit('setUser', newUser)
       })
       .catch(error => {
+        commit('setLoading', false)
+        commit('setError', error)
         console.log(error)
       })
   },
   signUserIn ({commit}, payload) {
+    commit('setLoading', true)
+    commit('clearError')
     firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
       .then(user => {
+        commit('setLoading', false)
         const newUser = {
           id: user.uid
         }
         commit('setUser', newUser)
       })
       .catch(error => {
+        commit('setLoading', false)
+        commit('setError', error)
         console.log(error)
       })
   }
